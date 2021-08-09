54066d967dd5a093f04f57ea51c1a020f6119fcb
Muhammad Adamu Sidi-ali <46903027+Sidi-Ali@users.noreply.github.com>
2021-08-09 22:52:48 +0100

    Add files via upload
    Initial files

---
+++ index.js
@@ -0,0 +1,56 @@
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++){
+  document.querySelectorAll("button")[i].addEventListener("click", function(){
+    drumSound(this.innerHTML);
+    animateButton(this.innerHTML);
+  });
+}
+
+document.addEventListener("keydown", function(event){
+  drumSound(event.key);
+  animateButton(event.key);
+})
+
+function drumSound(key){
+  switch (key) {
+    case "w":
+        var sound = new Audio("sounds/crash.mp3");
+        sound.play();
+      break;
+    case "a":
+         var sound = new Audio("sounds/kick-bass.mp3");
+         sound.play();
+      break;
+    case "s":
+         var sound = new Audio("sounds/snare.mp3");
+          sound.play();
+        break;
+    case "d":
+         var sound = new Audio("sounds/tom-1.mp3");
+           sound.play();
+        break;
+    case "j":
+         var sound = new Audio("sounds/tom-2.mp3");
+            sound.play();
+          break;
+    case "k":
+         var sound = new Audio("sounds/tom-3.mp3");
+             sound.play();
+          break;
+    case "l":
+         var sound = new Audio("sounds/tom-4.mp3");
+              sound.play();
+            break;
+
+    default:
+            console.log(this.innerHTML);
+ }
+}
+
+function animateButton(currentKey){
+  document.querySelector("." + currentKey).classList.add("pressed");
+
+  setTimeout(function(){
+    document.querySelector("." + currentKey).classList.remove("pressed");
+  }, 100);
+
+ }
